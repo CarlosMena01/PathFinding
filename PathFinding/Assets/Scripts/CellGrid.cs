60896f2396d5737c33016385697681efc09ca047
CarlosMena01 <cmena@unal.edu.co>
2022-06-18 13:26:51 -0500

    GridGenerator

--- PathFinding/Assets/Scripts/CellGrid.cs
+++ PathFinding/Assets/Scripts/CellGrid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CellGrid : MonoBehaviour
 {
+<<<<<<< HEAD
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,42 @@ public class CellGrid : MonoBehaviour
     {
         
     }
+=======
+    [SerializeField] private Color _colorBase, _colorWall, _colorInPath, _colorThePath;
+    
+    [SerializeField] private SpriteRenderer _renderer;
+
+    [SerializeField] private bool isWall, isInPath, isThePath;
+
+    public void Update() {
+        if(isWall) {
+            _renderer.color = _colorWall;
+
+        } else if(isInPath) {
+            if(isThePath) {
+                _renderer.color = _colorThePath;
+            } else {
+                _renderer.color = _colorInPath;
+            }
+        } else {
+            _renderer.color = _colorBase;
+        }
+    }
+
+    public void OnMouseDown() {
+        if(isWall) {
+            _renderer.color = _colorBase;
+            isWall = false;
+        } else {
+            _renderer.color = _colorWall;
+            isWall = true;
+        }
+        
+    }
+    public void State(bool isWall, bool isInPath, bool isThePath){
+        this.isWall     = isWall;
+        this.isInPath   = isInPath;
+        this.isThePath  = isThePath;
+    }
+>>>>>>> 15d67ae2 (GridGenerator)
 }
---
+++ PathFinding/Assets/Scripts/GridManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]  private int _width, _height;
+
+    [SerializeField]  private CellGrid _cellPrefab;
+
+    [SerializeField]  private Transform _cam;
+
+    private void Start() {
+        Generator();
+    }
+    
+    private void Update() {
+        GameObject cell = GameObject. 
+    }
+    private void Generator(){
+        for(int x = 0; x < _width; x++) {
+            for(int y = 0; y < _height; y++) {
+                var spawCell = Instantiate(_cellPrefab, new Vector3(x,y), Quaternion.identity);
+                spawCell.name = $"Cell_{x}_{y}";
+                spawCell.State(false, false, false);
+            }   
+        }
+
+        _cam.transform.position = new Vector3((float) _width/2 - 0.5f, (float) _height/2 - 0.5f, -100);
+    }
+
+}
