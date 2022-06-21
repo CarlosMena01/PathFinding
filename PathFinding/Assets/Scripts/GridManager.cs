38bc44b25470ffd58be14531e5ccc961622e162c
CarlosMena01 <cmena@unal.edu.co>
2022-06-21 09:54:31 -0500

    DFS try1

--- PathFinding/Assets/Scripts/GridManager.cs
+++ PathFinding/Assets/Scripts/GridManager.cs
@@ -14,8 +14,8 @@ public class GridManager : MonoBehaviour
 
     private void Start() {
         Generator();
-        Debug.Log("Empezamos");
-        MazeDFS("Cell_8_8");
+
+        MazeDFS("Cell_2_2");
     }
     
     private void Generator(){
@@ -47,52 +47,88 @@ public class GridManager : MonoBehaviour
     private void MazeDFS(string startCell){
         List<string>    neighborhoodCells   = NeighborhoodDFS(startCell);
         List<string>    visited             = new List<string>();
-        
+        List<string>    stack               = new List<string>();
 
-        CellGrid currentCell =  cellDict[neighborhoodCells[Random.Range(0, neighborhoodCells.Count)]];
-        CellGrid newCell;
-        visited.Add(currentCell.name);
+        CellGrid currentCell = cellDict[startCell];
+        CellGrid newCell = cellDict[neighborhoodCells[Random.Range(0, neighborhoodCells.Count)]]; //Seleccionamos un vecino al azar
+
+        visited.Add(startCell);
+        stack.Add(startCell);
+
+        //Generamos las celdas aisladas
+        for(int x = 0; x < _width; x++) {
+            for(int y = 0; y < _height; y++) {
+                currentCell = cellDict[$"Cell_{x}_{y}"];
+
+                bool isWall = !(x%2 == 0 && y%2 == 0); 
+                currentCell.State(isWall, false, false);
+            }   
+        }
 
         int count = 0;
 
-        while (currentCell.name != startCell)
+        while (true)
         {
-            neighborhoodCells   = NeighborhoodDFS(currentCell.name); //Actualizamos los nuevos vecinos
-            newCell = cellDict[neighborhoodCells[Random.Range(0, neighborhoodCells.Count)]]; //Seleccionamos un vecino al azar
+            Debug.Log(currentCell.name);
+            neighborhoodCells = NeighborhoodDFS(currentCell.name); // Actualizamos el vecindario
             
-            if(visited.Contains(newCell.name)){
-                //Si la celta ya había sido vistada, se cera un muro
+            //Eliminamos las celdas visitadas
+            for(int i = 0; i < neighborhoodCells.Count; i++) {
+                if(visited.Contains(neighborhoodCells[i])) {
+                    neighborhoodCells.Remove(neighborhoodCells[i]);
+                }                    
+            }
+
+            if(neighborhoodCells.Count >= 0) {
+                newCell = cellDict[neighborhoodCells[Random.Range(0, neighborhoodCells.Count)]]; //Seleccionamos un vecino al azar
+                //Si no ha sido visitada, creamos un puente entre ellas
                 if((newCell.getPosition() - currentCell.getPosition()).x > 0) {
+                    //La nueva celda está a la derecha
                     string name = $"Cell_{newCell.getPosition().x - 1}_{newCell.getPosition().y}";  
                     if(cellDict.ContainsKey(name)){
-                        cellDict[name].State(true,false,false);
+                        cellDict[name].State(false,false,false);
                     }
                     
                 } else if((newCell.getPosition() - currentCell.getPosition()).x < 0) {
+                    //La nueva celda está a la izquierda
                     string name = $"Cell_{newCell.getPosition().x + 1}_{newCell.getPosition().y}";  
                     if(cellDict.ContainsKey(name)){
-                        cellDict[name].State(true,false,false);
+                        cellDict[name].State(false,false,false);
                     }
                     
                 } else if((newCell.getPosition() - currentCell.getPosition()).y < 0) {
+                    //La nueva celda está abajo
                     string name = $"Cell_{newCell.getPosition().x}_{newCell.getPosition().y + 1}";  
                     if(cellDict.ContainsKey(name)){
-                        cellDict[name].State(true,false,false);
+                        cellDict[name].State(false,false,false);
                     }
                     
                 } else if((newCell.getPosition() - currentCell.getPosition()).y > 0) {
+                    //La nueva celda está arriba
                     string name = $"Cell_{newCell.getPosition().x}_{newCell.getPosition().y - 1}";  
                     if(cellDict.ContainsKey(name)){
-                        cellDict[name].State(true,false,false);
+                        cellDict[name].State(false,false,false);
                     }
-                    
                 }
-            } else {
-                currentCell = newCell;
-                visited.Add(currentCell.name);
+
+                //Añadimos la celda a las visitadas
+                visited.Add(newCell.name);
+                stack.Add(newCell.name);
+                currentCell = newCell; //Nos movemos a la nueva celda
+            } else{
+                currentCell = cellDict[stack[stack.Count - 1]];
+                stack.Remove(newCell.name);
             }
 
-            if(count >= _width * _height){
+            //Condiciones de parada
+            if(count >= _width * _height * 100){
+                Debug.Log($"Se recorrieron {count} celdas");
+                break;
+            }
+            if(currentCell.name == startCell){
+                break;
+            }
+            if(stack.Count <= 1){
                 break;
             }
             count++;
